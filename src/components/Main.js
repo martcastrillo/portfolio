af1c5bce1bd84eda99726a13e58b947cb01a2819
Marta Castrillo <mart.castrillo@gmail.com>
2023-03-01 00:34:12 +0100

    add updates

--- src/components/Main.js
+++ src/components/Main.js
@@ -13,16 +13,31 @@ function Main() {
 					<div className="landing-text-box">
 						<h1 className="main_landingtext">Marta Castrillo</h1>
 						<h2 className="landingtext">Porfolio</h2>
+						
 						<div className="landing_offices">
-							<NavLink to="/Dev">
+							Junior Web Developer, viendo la gran crecida de este sector y por
+							ser una persona curiosa me adentro en el mundo de la programación.
+							
+						</div>
+						<div className="landing_offices">
+							Soy editora de vídeo y diseñadora gráfica, también he realizado
+							otras labores como ayudante de dirección, ayudante de cámara y
+							producción según la necesidad. </div>
+						<div className="landing_offices">En otra vida también fui agente de viajes. <p></p></div>
+						<div className="landing_offices">
+							Soy una persona enérgica, trabajadora, colaboradora, con ganas de
+							aprender y siempre estoy muy motivada.
+						</div>
+						<div className="landing_offices_options">
+							 <NavLink to="/Dev">
 								<p className="mini_link">web developer</p>
 							</NavLink>
 							<NavLink to="/Video">
 								<p className="mini_link">video editor</p>
 							</NavLink>
-					{/* 		<NavLink to="/Graphic"> 
+							<NavLink to="/Graphic"> 
 								<p className="mini_link">graphic design</p>
-						</NavLink> */}
+						</NavLink>
 						</div>
 					</div>
 					<img className="landing-image" src={landing} alt="" />
